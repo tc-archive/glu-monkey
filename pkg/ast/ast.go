e1ef8edec751ac5d1b2f2d7e1d6c0a3bf55aff4e
Tim Langford <tim.langford@gmail.com>
2018-11-02 22:27:14 +0000

    Updated ast and parser.

---
+++ pkg/ast/ast.go
@@ -0,0 +1,69 @@
+package ast
+
+import "github.com/templecloud/go-glu/pkg/token"
+
+// Base Node ---------------------------------------------------------------
+//
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program Node ---------------------------------------------------------------
+//
+
+// Program is the root node of the AST.
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// LetStatement ---------------------------------------------------------------
+//
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {
+
+}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+// Identifier -----------------------------------------------------------------
+//
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {
+
+}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
---
+++ pkg/parser/parser.go
@@ -0,0 +1,30 @@
+package parser
+
+import (
+	"github.com/templecloud/go-glu/pkg/ast"
+	"github.com/templecloud/go-glu/pkg/lexer"
+	"github.com/templecloud/go-glu/pkg/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	// Read two tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
